3c6f90b53cb9c4184ca1d78c7a6b727045827751
AshrafMd-1 <21951A6612@iare.ac.in>
2022-11-19 10:36:40 +0530

    update app.py

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=False, port=os.getenv("PORT", default=5000))
